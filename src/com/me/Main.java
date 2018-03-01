d022201d6545156f8fbe55bba0739225cb8a4ee0
patrickatcw <pj8392@yahoo.com>
2018-03-01 11:20:08 -0500

    added method to return random movies

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -102,13 +102,37 @@ class Forgettable extends Movie {
 }
 
 
-
-
-
 public class Main {
 
     public static void main(String[] args) {
 
     }
 
+
+    //step 20 method to create in Main class, purpose is to
+    // return a random
+    public static Movie randomMovie() {
+        //step 21
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was: " + randomNumber);
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MaizeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgettable();
+
+        }
+
+        //step 22 return null
+        return null;
+
+    }
 }
+
+
