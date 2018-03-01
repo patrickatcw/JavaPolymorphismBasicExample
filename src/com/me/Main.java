db21724c761ba2cbffc7df2f10a4b36029ec8831
patrickatcw <pj8392@yahoo.com>
2018-03-01 10:46:40 -0500

    set up initial structure and updated gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,16 @@
+package com.me;
+//the ability of an object to take on many forms.
+// The most common use of polymorphism in OOP occurs
+// when a parent class reference is used to refer to a
+// child class object
+//no separate class files, everything is in 1 file
+
+//working with movies here.....
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
